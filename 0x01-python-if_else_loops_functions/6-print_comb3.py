f457f463ccb525c6e1b255d79732eebda7c87626
Abderrahmen Benjelloun <benjelloun857@gmail.com>
2023-11-01 08:21:45 -0700

    task7_v1

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+for i in range(0, 9):
+    for j in range(i+1, 10):
+        if i == 8:
+           print("{}{}".format(str(i), str(j))) 
+        else:
+            print("{}{}, ".format(str(i), str(j)), end="")
