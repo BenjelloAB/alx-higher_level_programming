23ffcae698a19a614c067181f5c1cb7e4a7b34a6
Abderrahmen Benjelloun <benjelloun857@gmail.com>
2023-11-01 08:58:31 -0700

    task9_v2

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,8 @@
+#!usr/bin/python3
+def uppercase(str):
+    for i in range(0,  len(str)):
+        if ord(str[i]) >= 97 and ord(str[i]) <= 122:
+            print("{}".format(chr(ord(str[i]) - 32)), end="")
+        else:
+            print("{}".format(str[i]), end="")
+    print("\n")
