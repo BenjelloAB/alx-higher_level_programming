67604d29eab37855c13823cbb3617626c6210307
Abderrahmen Benjelloun <benjelloun857@gmail.com>
2023-11-01 08:23:49 -0700

    task7_v2

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,6 @@
 for i in range(0, 9):
     for j in range(i+1, 10):
         if i == 8:
-           print("{}{}".format(str(i), str(j))) 
+            print("{}{}".format(str(i), str(j)))
         else:
             print("{}{}, ".format(str(i), str(j)), end="")
