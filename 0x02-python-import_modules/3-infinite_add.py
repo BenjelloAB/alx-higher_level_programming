490e1a53856dfe91a0736b6e044885e6feb248c3
Abderrahmen Benjelloun <benjelloun857@gmail.com>
2023-11-02 18:01:14 -0700

    task4_v2

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -2,6 +2,6 @@
 if __name__ == "__main__":
     from sys import argv
     s = 0
-    for i in range(1, len(argv) + 1):
-        sum += int(argv[i])
+    for i in range(1, len(argv)):
+        s += int(argv[i])
     print("{:d}".format(s))
