1b8441b36211518595fd7758466735a8f3565ea1
Abderrahmen Benjelloun <benjelloun857@gmail.com>
2023-11-02 19:18:43 -0700

    task100_v1

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -6,6 +6,9 @@ if __name__ == "__main__":
     a = sys.argv[:]
     e1 = int(a[1])
     e2 = int(a[3])
+    if lena(a) == 1:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
     if a[2] == "+":
         print("{} {} {} = {:d}".format(a[1], a[2], a[3], calc.add(e1, e2)))
         sys.exit(0)
