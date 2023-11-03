9f5e8c7f14fbd193bc4caa5618dd81060c41c7f5
Abderrahmen Benjelloun <benjelloun857@gmail.com>
2023-11-02 17:56:03 -0700

    task3_v1

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,7 +2,7 @@
 if __name__ == "__main__":
     from sys import argv
     size = len(argv) - 1
-    if size  == 0:
+    if size == 0:
         print("{} arguments.".format(size))
     elif size > 0:
         print("{} argument{}:".format(size, "s" if size > 1 else ""))
