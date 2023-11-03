6c722c641e9238d6deff91db9985b8663dd2d89e
Abderrahmen Benjelloun <benjelloun857@gmail.com>
2023-11-02 17:30:30 -0700

    task1_v1

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-import calculator_1 as calc
 if __name__ == "__main__":
+    import calculator_1 as calc
     a = 10
     b = 5
     print("{} + {} = {}".format(a, b, calc.add(a, b)))
