5eef0ef2032af61112eaa1824796118cfab414ef
Abderrahmen Benjelloun <benjelloun857@gmail.com>
2023-11-02 17:31:15 -0700

    task0_32

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import add_0 as mod
-
-a = 1
-b = 2
-print("{:d} + {:d} = {:d}".format(a, b, mod.add(a, b)))
+    a = 1
+    b = 2
+    print("{:d} + {:d} = {:d}".format(a, b, mod.add(a, b)))
