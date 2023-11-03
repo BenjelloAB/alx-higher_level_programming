780b7d1c2b3b1fe433fc08bd5b934c2438e9ebd3
Abderrahmen Benjelloun <benjelloun857@gmail.com>
2023-11-02 19:22:12 -0700

    task100_v1

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -4,23 +4,24 @@ if __name__ == "__main__":
     import calculator_1 as calc
 
     a = sys.argv[:]
-    e1 = int(a[1])
-    e2 = int(a[3])
-    if lena(a) == 1:
+    if len(a) == 1:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
-    if a[2] == "+":
-        print("{} {} {} = {:d}".format(a[1], a[2], a[3], calc.add(e1, e2)))
-        sys.exit(0)
-    elif a[2] == "-":
-        print("{} {} {:d} = {:d}".format(a[1], a[2], a[3], calc.sub(e1, e2)))
-        sys.exit(0)
-    elif a[2] == "*":
-        print("{} {} {} = {}".format(a[1], a[2], a[3], calc.mul(e1, e2)))
-        sys.exit(0)
-    elif a[2] == "/":
-        print("{} {} {} = {}".format(a[1], a[2], a[3], calc.div(e1, e2)))
-        sys.exit(0)
-    else:
-        print("Unknown operator. Available operators: +, -, * and /")
-        sys.exit(1)
+    elif len(a) > 1:
+        e2 = int(a[3])
+        e1 = int(a[1])
+        if a[2] == "+":
+            print("{} {} {} = {:d}".format(a[1], a[2], a[3], calc.add(e1, e2)))
+            sys.exit(0)
+        elif a[2] == "-":
+            print("{} {} {:d} = {:d}".format(a[1], a[2], a[3], calc.sub(e1, e2)))
+            sys.exit(0)
+        elif a[2] == "*":
+            print("{} {} {} = {}".format(a[1], a[2], a[3], calc.mul(e1, e2)))
+            sys.exit(0)
+        elif a[2] == "/":
+            print("{} {} {} = {}".format(a[1], a[2], a[3], calc.div(e1, e2)))
+            sys.exit(0)
+        else:
+            print("Unknown operator. Available operators: +, -, * and /")
+            sys.exit(1)
