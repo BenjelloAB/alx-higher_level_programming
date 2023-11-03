10bca3f25fb9f28bf23c5d000bada205174abba0
Abderrahmen Benjelloun <benjelloun857@gmail.com>
2023-11-02 17:55:24 -0700

    task3_v1

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+    size = len(argv) - 1
+    if size  == 0:
+        print("{} arguments.".format(size))
+    elif size > 0:
+        print("{} argument{}:".format(size, "s" if size > 1 else ""))
+        for i in range(1, size + 1):
+            print("{}: {}".format(i, argv[i]))
