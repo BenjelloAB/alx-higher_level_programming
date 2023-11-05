3d9c57f9001511762e2fd32454afc85b2e35069f
Abderrahmen Benjelloun <benjelloun857@gmail.com>
2023-11-05 03:23:02 -0800

    task6_v2

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
+
 def no_c(my_string):
     if my_string:
         str = ""
-        for i in range(0, len(my_string)):
-            if my_string[i] != 'c' and my_string[i] != 'C':
-                str += my_string[i]
-        return str
+        for m in my_string:
+            if m != 'c' and m != 'C':
+                str += m
+        return (str)
