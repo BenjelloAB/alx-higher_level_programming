648ad3e429369c392064321f1e7ac0864c1ca368
Abderrahmane <benjelloun857@gmail.com>
2023-11-09 00:50:55 +0000

    task1_v3

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 
 def search_replace(my_list, search, replace):
-    index = my_list.index(search)
     new_list = my_list[:]
-    new_list[index] = replace
+    for i in range(len(new_list)):
+        if new_list[i] == search:
+            new_list[i] = replace
     return new_list
