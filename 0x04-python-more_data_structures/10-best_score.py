5efea34d8e5ef3a802cc23a3210d7f28e57e8518
Abderrahmane <benjelloun857@gmail.com>
2023-11-09 02:49:29 +0000

    task11_v2.2

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,17 +1,19 @@
 #!/usr/bin/python3
 
 def max_items(items):
-    maxy = items[0][1]
-    for i in range(1, len(items)):
-        if maxy < items[i][1]:
-            maxy = items[i][1]
-            name = items[i][0]
-    return name
+    max_name, max_score = items[0]
+    for name, score in items:
+        if max_score < score:
+            max_score = score
+            max_name = name
+    return max_name
 
 
 def best_score(a_dictionary):
     if a_dictionary is None:
         return None
     items = list(a_dictionary.items())
+    if not items:
+        return None
     info_tuple = max_items(items)
     return info_tuple
