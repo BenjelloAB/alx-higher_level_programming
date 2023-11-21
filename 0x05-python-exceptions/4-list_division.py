900614fc162c3dc4a2df480cfdbf5d904941ad16
Abderrahmen Benjelloun <benjelloun857@gmail.com>
2023-11-20 16:41:36 -0800

    task5_v1

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+def list_division(my_list_1, my_list_2, list_length):
+    res = []
+    for i in range(list_length):
+        try:
+            res.append(my_list_1[i] / my_list_2[i])
+        except ZeroDivisionError:
+            print("division by 0")
+            res.append(0)
+        except TypeError:
+            print("wrong type")
+            res.append(0)
+        except IndexError:
+            print("out of range")
+            res.append(0)
+    return (res)
