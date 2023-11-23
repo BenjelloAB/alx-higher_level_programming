4c76336cd68f1d10ac9c014d17b63694425d4c97
Abderrahmen Benjelloun <benjelloun857@gmail.com>
2023-11-22 18:19:29 -0800

    task6_fix1

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -5,6 +5,7 @@
 
 class Square:
     """Representation of a square"""
+
     def __init__(self, size=0, position=(0, 0)):
         """Initialize the obj
 
@@ -61,11 +62,8 @@ class Square:
         if self.__size == 0:
             print()
         else:
-            for i in range(0, self.__position[1]):
-                print("")
-            for i in range(self.__size):
-                for k in range(self.__position[0]):
-                    print(" ", end="")
-                for j in range(self.__size):
-                    print("#", end="")
-                print()
+            [print("") for i in range(0, self.__position[1])]
+            for i in range(0, self.__size):
+                [print(" ", end="") for j in range(0, self.__position[0])]
+                [print("#", end="") for k in range(0, self.__size)]
+            print("")
