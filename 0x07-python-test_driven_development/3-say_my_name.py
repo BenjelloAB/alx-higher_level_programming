778ead7f35c8100be3c7b79a32941c96beb53306
Abderrahmen Benjelloun <benjelloun857@gmail.com>
2023-12-01 14:27:14 -0800

    task3_v1

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -4,6 +4,7 @@ This moduledefines a function that:
 prints My name is <first name> <last name>
 """
 
+
 def say_my_name(first_name, last_name=""):
     """
     a function that prints formatted string with first
