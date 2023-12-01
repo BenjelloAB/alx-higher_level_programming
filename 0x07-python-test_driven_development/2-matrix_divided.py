62c0d2d786357bc7f647a9e26b5c1822ced20bae
Abderrahmen Benjelloun <benjelloun857@gmail.com>
2023-12-01 09:27:32 -0800

    task2_v2

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -15,22 +15,32 @@ def matrix_divided(matrix, div):
         Raises:
             TypeError (1): if the elements aren't floats or ints
             TypeError (2): if the rows aren't equal size wise
+            TypeError (3): if the div is not an int or float type
             ZeroDivisionError: if we are dividing by 0
     """
+    if type(div) not in (int, float):
+        raise TypeError("div must be a number")
 
     if div == 0:
         raise ZeroDivisionError("division by zero")
+
+    str = "matrix must be a matrix (list of lists) of integers/floats"
+
+    if len(matrix) == 0:
+        raise TypeError(str)
+    for i in range(len(matrix)):
+        if len(matrix[i]) == 0:
+            raise TypeError(str)
+
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            if type(matrix[i][j]) not in (int, float):
+                raise TypeError(str)
 
     row_len = len(matrix[0])
     for i in range(len(matrix)):
         if len(matrix[i]) != row_len:
             raise TypeError("Each row of the matrix must have the same size")
-
-    str = "matrix must be a matrix (list of lists) of integers/floats"
-    for i in range(len(matrix)):
-        for j in range(row_len):
-            if type(matrix[i][j]) not in (int, float):
-                raise TypeError(str)
 
     new_matrix = []
     sub_list = []
