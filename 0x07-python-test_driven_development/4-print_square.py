a07df611b5d9ba41d7634d693939364762220716
Abderrahmen Benjelloun <benjelloun857@gmail.com>
2023-12-01 15:25:15 -0800

    task5_v1

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+
+"""
+
+
+def print_square(size):
+    """
+    a function that prints a square of size cols 
+    and size rows
+
+    Args:
+        size : the length of the square
+
+    Raises:
+        TypeError:   if the size is not an integer
+        TypeError: if size is a float or is less than 0
+        ValueError: when size is less than 0
+    """
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+
+    if size < 0:
+        raise ValueError("size must be >= 0")
+
+    for i in range(size):
+        for j in range(size):
+            print('#', end="")
+        print()
