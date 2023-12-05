9bd65c37885498bef24b0872411d17bd07302790
Abderrahmen Benjelloun <benjelloun857@gmail.com>
2023-12-05 15:27:31 -0800

    task1_v1

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin./python3
+'''This module defines a method that write a string to a file'''
+
+
+def write_file(filename="", text=""):
+    """
+    This function write some text to a file
+
+    Args:
+        filename: the file name
+        text: string to write into the file
+    """
+    with open(filename, "w", encoding="utf-8") as f:
+        return f.write(text)
