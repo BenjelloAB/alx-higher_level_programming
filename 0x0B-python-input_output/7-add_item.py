8632f0c265b592966b8db288607cb02673b70ee5
Abderrahmen Benjelloun <benjelloun857@gmail.com>
2023-12-05 17:49:47 -0800

    task7_v1

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
+'''A Module that loads and dumps'''
 import sys
 
-save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+if __name__ == "__main__":
+    save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+    load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 try:
     items = load_from_json_file("add_item.json")
