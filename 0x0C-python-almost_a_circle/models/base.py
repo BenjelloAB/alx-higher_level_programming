b8178095deacdcaa1373b81c457a875c6fa24ae7
Abderrahmen Benjelloun <benjelloun857@gmail.com>
2023-12-14 14:07:16 -0800

    tasks_v5

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """Module that defines a Base class"""
+import json
 
 
 class Base:
@@ -15,3 +16,31 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """
+        serialize the data into a json string
+        """
+        return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """
+        Serialize the data into a json string
+        and save it in a file
+        """
+        list_dict = []
+        if list_objs is not None:
+            for item in list_objs:
+                dic_it = item.to_dictionary()
+                list_dict.append(dic_it)
+            with open("{}.json".format(cls.__name__), "w") as fd:
+                json.dump(list_dict, fd)
+
+    @staticmethod
+    def from_json_string(json_string):
+        """
+        Returns the list of the JSON string representationN
+        """
+        return json.loads(json_string)
