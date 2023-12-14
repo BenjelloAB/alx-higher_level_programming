d14b0baa95225c2afeb782c0e40a88d74c7643e4
Abderrahmen Benjelloun <benjelloun857@gmail.com>
2023-12-14 09:01:42 -0800

    task9_v1

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Module that defines Square class which
+inherit from Rectangle class"""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """
+    Square class , a subclass of Rectangle
+    """
+
+    def __init__(self, size, x=0, y=0, id=None):
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        s = self.height
+        ed = self.id
+        x = self.x
+        y = self.y
+        n = self.__class__.__name__
+        return "[{}] ({}) {}/{} - {}".format(n, ed, x, y, s)
