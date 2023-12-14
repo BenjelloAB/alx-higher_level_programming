1958314be1b84d309bf4609921cc69476c16199a
Abderrahmen Benjelloun <benjelloun857@gmail.com>
2023-12-13 23:49:57 -0800

    tasks

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -23,10 +23,7 @@ class Rectangle(Base):
 
     @width.setter
     def width(self, value):
-        if not isinstance(value, int):
-            raise TypeError("width must be an integer")
-        if value < 0:
-            raise ValueError("width must be >= 0")
+        self.__class__.checker("width", value)
         self.__width = value
 
     @property
@@ -35,10 +32,7 @@ class Rectangle(Base):
 
     @height.setter
     def height(self, value):
-        if not isinstance(value, int):
-            raise TypeError("height must be an integer")
-        if value < 0:
-            raise ValueError("height must be >= 0")
+        self.__class__.checker("height", value)
         self.__height = value
 
     @property
@@ -47,10 +41,7 @@ class Rectangle(Base):
 
     @x.setter
     def x(self, value):
-        if not isinstance(value, int):
-            raise TypeError("x must be an integer")
-        if value < 0:
-            raise ValueError("x must be >= 0")
+        self.__class__.checker("x", value)
         self.__x = value
 
     @property
@@ -59,8 +50,28 @@ class Rectangle(Base):
 
     @y.setter
     def y(self, value):
-        if not isinstance(value, int):
-            raise TypeError("y must be an integer")
-        if value < 0:
-            raise ValueError("y must be >= 0")
+        self.__class__.checker("y", value)
         self.__y = value
+
+    @staticmethod
+    def checker(name, value):
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        if value < 0:
+            raise ValueError("{} must be >= 0".format(name))
+
+    def area(self):
+        return self.width * self.height
+
+    def display(self):
+        for i in range(self.height):
+            for j in range(self.width):
+                print("#",end="")
+            print()
+
+    def __str__(self):
+        return "[{}] ({}) {}/{} - {}/{}".format(self.__class__.__name__, 
+                self.id, self.x, self.y, self.width, self.height)
+
+
+        
