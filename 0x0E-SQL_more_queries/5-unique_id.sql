eb8eb4fe50309b76bbbf2719ff1cf39b409622dc
Abderrahmen Benjelloun <benjelloun857@gmail.com>
2024-01-18 17:20:29 -0800

    tasks_v2.2

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -1,3 +1,3 @@
--- creates the table id_not_null
+-- creates the table unique_id
 
-CREATE TABLE IF NOT EXISTS id_not_null (id INT DEFAULT 1, name VARCHAR(256));
+CREATE TABLE IF NOT EXISTS unique_id (id INT DEFAULT 1, name VARCHAR(256));
