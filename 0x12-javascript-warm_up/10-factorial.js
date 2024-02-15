877887a44b4253532bb19cd165311e30e73809b8
Abderrahmen Benjelloun <benjelloun857@gmail.com>
2024-02-14 17:49:13 -0800

    fixes version1.1

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,8 +1,12 @@
 #!/usr/bin/node
 function factorial(number)
 {
-	if(isNaN(number) || number == 1)
-		return 1;
-	return number * factorial(number - 1);
+	if(n < 0)
+	{
+		return (-1);
+	}
+	if(isNaN(number) || number === 0)
+		return (1);
+	return (number * factorial(number - 1));
 }
-console.log(factorial(Number(process.argv[2])))
+console.log(factorial(Number(process.argv[2])));
