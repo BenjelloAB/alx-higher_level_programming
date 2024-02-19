508607e7e4abb575b64e3df20380e1a897fbf508
Abderrahmen Benjelloun <benjelloun857@gmail.com>
2024-02-19 12:34:53 -0800

    fixes v1.2

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,4 +1,2 @@
 #!/usr/bin/node
-if (process.argv.length === 2) { console.log('No argument'); } else {
-  for (let i = 2; i < process.argv.length; i++) { console.log(process.argv[i]); }
-}
+if (process.argv[2] === undefined) { console.log('No argument'); } else { console.log(process.argv[2]); }
